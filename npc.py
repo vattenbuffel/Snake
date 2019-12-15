8a80d515369197e477521cb56c4d84855e41351e
Noa <nois97.noa@gmail.com>
2019-12-15 20:25:39 +0100

    added qlearning functions to npc

--- npc.py
+++ npc.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 import math
+import tensorflow
+from keras.models import Sequential
+import numpy as np
 
 
 class NPC:
@@ -17,6 +20,11 @@ class NPC:
         self.frame_delay = self.frame_delay_max
         self.eaten = False
         self.end_pos = None
+        self.old_distance_to_food = -1
+        self.new_distance_to_food = -1
+        self.old_state = [-1 for x in game.squares]
+        self.new_state = [-1 for x in game.squares]
+        self.reward = -1
 
     def eat(self, game):
         if self.xs[0] == game.food.x and self.ys[0] == game.food.y:
@@ -34,18 +42,46 @@ class NPC:
     def render(self):
         pass
 
+    def update_state(self, game):
+        self.old_state = self.new_state
+        self.old_state = np.array([x.state for x in game.squares])
+
+    def update_dist(self, game):
+        self.old_distance_to_food = self.new_distance_to_food
+        self.new_distance_to_food = self.calc_dist_to_food(game)
+
+    def update_reward(self, game):
+        # If snake died reward is -10^6
+        if not self.alive:
+            self.reward = -10**6
+
+        # If food has been eaten reward is 10^3
+        elif self.eaten:
+            self.reward = 10 ** 3
+
+        # Check if gotten closer to food reward is 1 and update old_dist_to_food
+        elif self.new_distance_to_food < self.old_distance_to_food:
+            self.reward = 1
+            # If not closer reward is -1
+        else:
+            self.reward = -1
+
+    def calc_dist_to_food(self, game):
+        x_diff = self.xs[0] - game.food.x
+        y_diff = self.ys[0] - game.food.y
+        dist = (x_diff**2 + y_diff**2)**0.5
+        return dist
+
     def update(self, game):
         self.frame_delay -= 1
         self.eat(game)
         self.move(game.events)
 
-        if self.eaten and self.frame_delay == 0:
-            self.frame_delay_max -= 1
-            if self.frame_delay_max <= 0:
-                self.frame_delay_max = 1
-
-        self.reproduce()
         self.die()
+        self.reproduce()
+        self.update_dist(game)
+        self.update_state(game)
+        self.update_reward(game)
         self.square_update()
         if self.frame_delay <= 0:
             self.frame_delay = self.frame_delay_max
